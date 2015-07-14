dc6e8c0bbedd48a5cda915ece501edba90498804
bemmerink <bemmerink@sogyo.nl>
2015-07-14 17:33:30 +0200

    Hello World

--- src/main/java/nl/sogyo/game/Game.java
+++ src/main/java/nl/sogyo/game/Game.java
@@ -4,4 +4,12 @@ package nl.sogyo.game;
  * Created by bemmerink on 14-7-2015.
  */
 public class Game {
+
+    public static void main(String[] args) {
+        new Game();
+    }
+
+    public Game() {
+        System.out.println("Hello World!");
+    }
 }
